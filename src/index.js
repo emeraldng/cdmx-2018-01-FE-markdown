49e4a5b5698dd493ab32f4de084a05ac92394295
emeraldng <ifglitterthen@gmail.com>
2018-08-31 12:43:39 -0500

    return arr

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-console.log('badhbs');
+const fs = require('fs');
+// var showdown  = require('showdown');
+// let converter = new showdown.Converter(),
+//  text = ;
+//  html = converter.makeHtml(text);
+    
+//const https = require('https');
+// let filepath = 'README.html';
+
+fs.readFile('../README.md', 'utf8', (error, data) =>{
+    let content = data;
+    if(error){
+        console.log(`Error ${error}`);
+    }else{
+        console.log(content);
+        linkarr(content);
+    }
+    
+});
+
+const linkarr = (content) =>{
+    let regex = /((http:\/\/|https:\/\/|www\.)[^\s]+)/gim;
+    let matchstr = content.match(regex);
+    let url = matchstr;
+    console.log(url);
+};
 
