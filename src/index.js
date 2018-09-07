031bd4cf953769284f49d0092329ad6cbfd96c6e
emeraldng <ifglitterthen@gmail.com>
2018-09-07 01:50:36 -0500

    loop para str

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 
-fs.readFile('../PRUEBA.md', 'utf8', (error, data) => {
+fs.readFile('../.md', 'utf8', (error, data) => {
     let content = data;
     if (error) {
         console.log(`Error ${error}`);
@@ -11,8 +11,22 @@ fs.readFile('../PRUEBA.md', 'utf8', (error, data) => {
         //console.log(resarrs);
         const strStr = resarrs.filter(resarr => {
             return resarr.includes('(http');
-            
         });
         console.log(strStr);
+        // for (const x of strStr) {
+        //     if (x.length === 0) break;
+        //     console.log(x);
+        //}
     }
 });
+
+// var txt = 'herramienta usando [Node.js](https://nodejs.org/), que lea y analice archivos';
+// var newTxt = txt.split('(');
+// var arrPseudoUrls = [];
+// //console.log(newTxt);
+
+// for (var i = 1; i < newTxt.length; i++) {
+//     var pseudoURL = newTxt[i].split(')')[0];
+//     arrPseudoUrls.push(pseudoURL);
+//     console.log(arrPseudoUrls);
+// }
