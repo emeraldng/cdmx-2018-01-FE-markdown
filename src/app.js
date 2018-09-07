cdc8853fed86f32a18af53208aefcea5f2be8136
emeraldng <ifglitterthen@gmail.com>
2018-09-07 01:54:05 -0500

    Añadiendo Jest, pruenas, coverage

--- src/app.js
+++ src/app.js
@@ -1,2 +1,47 @@
 
-console.log('ffas');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var txt = 'herramienta usando [Node.js](https://nodejs.org/), que lea y analice archivos';
+// var newTxt = txt.split('(');
+// var arrPseudoUrls = [];
+// //console.log(newTxt);
+
+// for (var i = 1; i < newTxt.length; i++) {
+//     var pseudoURL = newTxt[i].split(')')[0];
+//     arrPseudoUrls.push(pseudoURL);
+//     console.log(arrPseudoUrls);
+// }
+
+
+
+
+
---
+++ tes-map.spec.js
@@ -0,0 +1,12 @@
+
+
+const { map, myCallbacks } = requiere('../test-map');
+
+describe('map', () => {
+    test('Debería map ser una función', () => {
+        expect(typeof map).toEqual('function');
+    });
+    test('Debería return un array si se le pasa un array', () =>{
+        expect(map[], [] myCallbacks) instanceof
+    })
+})
