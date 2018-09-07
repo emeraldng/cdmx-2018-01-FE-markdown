84b0269b407a1dde2e2acb65c31effaa166ed665
emeraldng <ifglitterthen@gmail.com>
2018-09-07 03:59:00 -0500

    separador

--- src/app.js
+++ src/app.js
@@ -3,54 +3,32 @@ const fs = require ('fs');
 //const path = require('path');
 const filepath = ('../PRUEBA.md');
 
-const readFileAsync = (filepath) => {
+fs.readFileAsync = (filepath) => {
     return new Promise((resolve, reject) => {
         fs.readFile(filepath, 'utf8', (err, data) => {
-            if(err) reject(err);
-            else resolve(data);
+            if(err){ 
+                reject(err);}
+            else{
+                const mkdata = data;
+                resolve(mkdata);}
         });
     });
 };
 
-readFileAsync(filepath)
-    .then((contents) =>{
-        console.log(contents);
+const separateByLineBreak = (mkdata) => {
+    const separator = mkdata.split('\r\n');
+    return separator; 
+};
+
+
+
+fs.readFileAsync(filepath)
+    .then((mkdata) =>{
+        //console.log(mkdata);
+        separateByLineBreak(mkdata);
+        console.log(separateByLineBreak);
     })
     .catch((err) =>{
         console.log('err!');
         
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
